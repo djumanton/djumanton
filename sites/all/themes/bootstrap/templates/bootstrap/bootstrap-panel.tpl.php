f010addf16535c681fd21d4e77d455ee5566f34a
djumanton <djumanton@mail.ru>
2016-04-01 00:44:54 +0300

    initial commit

---
+++ sites/all/themes/bootstrap/templates/bootstrap/bootstrap-panel.tpl.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file
+ * Default theme implementation to display a Bootstrap panel component.
+ *
+ * @todo Fill out list of available variables.
+ *
+ * @ingroup templates
+ */
+?>
